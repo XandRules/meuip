e49eee2c8ecf191bcd0a0b4554bf005ef5249074
Alexandre <alexandreribeiro@procobservicos.com.br>
2021-04-05 20:33:52 -0300

    feat: add busca reversa de localização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,36 @@ import { MapsService } from './maps.service';
 import { Component, OnInit } from '@angular/core';
 import { Location } from './maps';
 
+interface Position {
+  country:string;
+  country_code:string;
+  postcode:string;
+  restaurant:string;
+  road:string;
+  state:string;
+  state_district:string;
+  suburb:string;
+  town:string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   title = 'My first AGM project';
   lat = 51.678418;
   lng = 7.809007;
 
-  latitude: string = '';
-  longitude: string = '';
+  latitude: number;
+  longitude: number;
 
   location: Location = null;
+  position: Position;
 
-  constructor(private map: MapsService){
+  constructor(private map: MapsService) {
   }
 
 
@@ -27,12 +40,35 @@ export class AppComponent implements OnInit{
     this.map.getLocation().subscribe((data: Location) => {
       console.log(data);
 
-      this.latitude = data.latitude;
-      this.longitude = data.longitude;
       this.location = data;
+
+     this.getUserLocation();
     })
   }
 
+  getLocation(latitude: number, longitude: number) {
+    this.map.getLocationReverse(latitude, longitude).subscribe(
+      (data) => {
+        this.position = data.address;
+      }
+    )
+  }
+
+  getUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+
+        this.getLocation(this.lat, this.lng);
+      },
+      error => console.log(error),
+      {enableHighAccuracy:true, maximumAge:30000, timeout:27000}
+      );
+    } else {
+
+    }
+  }
 
 
 }
--- src/app/maps.service.ts
+++ src/app/maps.service.ts
@@ -14,4 +14,8 @@ export class MapsService {
   getLocation() : Observable<Location>{
     return this.http.get<Location>(`http://api.ipapi.com/api/check?access_key=${environment.IP_API_KEY}`);
   }
+
+  getLocationReverse(latitude: number, longitude: number): Observable<any>{
+    return this.http.get<any>(`https://us1.locationiq.com/v1/reverse.php?key=pk.a7e64683f0208bfabb1d8178580fd672&lat=${latitude}&lon=${longitude}&format=json`);
+  }
 }
